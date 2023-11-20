e27cd02f8773ccfe77535f711dfaba33b2e446bd
Hazem <h.hussien@singularity.finance>
2023-11-20 20:30:19 +0200

    Update price box settings and styles

--- src/edit.js
+++ src/edit.js
@@ -1,7 +1,6 @@
 import { useBlockProps, RichText, InspectorControls, ColorPalette } from '@wordpress/block-editor';
 import { PanelBody, TextControl, SelectControl } from '@wordpress/components';
 import { Button } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
 import './editor.scss';
 
 export default function Edit({ attributes, setAttributes }) {
@@ -10,29 +9,26 @@ export default function Edit({ attributes, setAttributes }) {
   return (
     <div {...useBlockProps()}>
       <InspectorControls>
-        <PanelBody title={__('Price Box Settings', 'price-box')}>
-          <TextControl
-            label={__('Plan Name', 'price-box')}
-            value={planName}
-            onChange={(newPlanName) => setAttributes({ planName: newPlanName })}
-          />
-          <TextControl label={__('Price', 'price-box')} value={price} onChange={(newPrice) => setAttributes({ price: newPrice })} />
-          <TextControl
-            label={__('Benefits', 'price-box')}
-            value={benefits}
-            onChange={(newBenefits) => setAttributes({ benefits: newBenefits })}
-          />
-          <TextControl
-            label={__('Discount', 'price-box')}
-            value={discount}
-            onChange={(newDiscount) => setAttributes({ discount: newDiscount })}
-          />
-          <TextControl label={__('Link', 'price-box')} value={link} onChange={(newLink) => setAttributes({ link: newLink })} />
+        <PanelBody title={'Price Box Settings'}>
+          <TextControl label={'Plan Name'} value={planName} onChange={(newPlanName) => setAttributes({ planName: newPlanName })} />
+          <TextControl label={'Price'} value={price} onChange={(newPrice) => setAttributes({ price: newPrice })} />
+          <TextControl label={'Benefits'} value={benefits} onChange={(newBenefits) => setAttributes({ benefits: newBenefits })} />
+          <TextControl label={'Discount'} value={discount} onChange={(newDiscount) => setAttributes({ discount: newDiscount })} />
+          <TextControl label={'Link'} value={link} onChange={(newLink) => setAttributes({ link: newLink })} />
         </PanelBody>
       </InspectorControls>
       <div className="price-box-container">
         <RichText.Content tagName="h2" value={planName} />
-        <RichText.Content tagName="h1" value={`$${price}`} />
+        <div
+          className="price-tag-wrapper"
+          style={{
+            right: discount ? '17px' : '0px',
+            position: discount ? 'relative' : '',
+          }}
+        >
+          {discount && <RichText.Content className="price-tag-initial" tagName="p" value={`$${discount}`} />}
+          <RichText.Content className="price-tag" tagName="h1" value={`$${price}`} />
+        </div>
         {benefits && benefits.length > 0 && (
           <ul className="price-box-ul">
             {benefits.map((benefit) => (
@@ -40,7 +36,6 @@ export default function Edit({ attributes, setAttributes }) {
             ))}
           </ul>
         )}
-        <RichText.Content tagName="p" value={discount} />
         <a href={link}>
           <Button variant="secondary">Buy Now</Button>
         </a>
