0b07aff31cd39b8254795e99f978dcfdea3cd151
Hazem <h.hussien@singularity.finance>
2023-11-21 20:20:34 +0200

    mutli thteme

--- readme.md
+++ readme.md
@@ -4,44 +4,70 @@ November 20, 2023
 
 ### **custom block for price box**
 
+### WordPress Custom Block: Price Box
+
 A dynamic block type, using React for Editor and frontend visible component
 
-![](https://telegra.ph/file/2aa22ed82c3dabfa4f40f.png)
+The block will conclude of a parent block that hosts by default 3 price blocks/3 plans
 
-### Block Attributes:
+1. Customizable:
+   This block is highly customizable. It allows users to set various attributes such as plan name, price, benefits, discount, and link. It also provides options to feature a plan, add a dotted list, and align the list.
+
+2. Interactive:
+   The block is interactive. It provides a user-friendly interface in the block editor for setting the attributes. It also allows users to toggle between different settings, enhancing the user experience.
+
+3. Dynamic:
+   The block is dynamic. It changes its appearance based on the attributes set by the user. For example, it shows a featured tag if the plan is featured, and it shows a discount price if a discount is set.
+
+4. Responsive:
+   The block is responsive. It adjusts its layout and style based on the screen size, ensuring a good user experience on all devices.
+
+5. Easy to Use:
+   The block is easy to use. It provides a simple and intuitive interface in the block editor, making it easy for users to create and customize their price box.
+
+6. Clean Code:
+   The block is developed with clean and efficient code. It uses modern JavaScript and CSS techniques, ensuring good performance and maintainability.
+
+#### inspirations
+
+<img src="https://telegra.ph/file/2aa22ed82c3dabfa4f40f.png" style="max-width:120px" alt="Image 1">
+<img src="http://tstore.thezed.me/i/600mcsv.png?s" style="max-width:120px" alt="Image 2">
+<img src="https://tstore.thezed.me/i/w31sh4j.png" style="max-width:120px" alt="Image 4">
+<img src="http://tstore.thezed.me/i/yznrq1u.png?s" style="max-width:120px" alt="Image 3">
+
+## _Our block_
+
+<img src="http://tstore.thezed.me/i/cbuo1b2.png" alt="Image 3" style="max-width: 600px;">
+
+### features:
+
+- Add/Remove to the benifts list
+- Choose between a dotted list or a normal list
+- Control font size, color, and alignment of price tag AND plan name
+- Set custom description for the plan
+- Set a block to be featured, will show a featured tag and give the block a unique eye catchy style
+- Select placement of Plan Name, top or bottom
+- Set a previous price and a discount, will show a line through the previous price and show the discount
+- Block Settings can be edited from side panel or directly from the block
+
+---
+
+## Dev Section
+
+### Single Block Attributes:
 
 ```
 Plan name :string
 Price: number
-Benefits: Arary[string] // array of strings, will be wrapped to show list
-Discount: number | default: null // will show with line through, to show
-LINK: string // url to direct user to on click
+Benefits: Array[string] // array of strings, will be wrapped to show list
+Discount: number | default: null // will show with line through, to show price before and after discount
+LINK: string // URL to direct user to on click
 Sections: array[object]
-// sections example:
-// [
-//   "top":{
-//     'background-color': '#fff',
-//     percentage: 50, // what percentage the section should take from the height of the block
-//   },
-//   "bottom":{
-//     'background-color': '#fff',
-//     percentage: 50,
-//   },
-// ];
 ```
 
-#### \- Block Attributes can be edited from side panel or directly from the block
+### Parent Block Attributes:
 
-**\-Initial packages:**
-
-**@wordpress/data @wordpress/components @wordpress/element @wordpress/block-editor @wordpress/scripts**
-
-**styling using scss**
-
-## Technical prereq:
-
-- **Enqueue Block Editor Scripts: enqueue JavaScript and CSS for the block editor. Use the wp_enqueue_script and wp_enqueue_style functions.**
-- **Register a New Block Type: register a new block type using the registerBlockType function. This function takes the name of the block and an object with configuration options.**
-- **Define Block Attributes: Define the attributes. For the price box, we'll need attributes for plan name, price, benefits, discount, link, and sections.**
-- **Create Edit Function: This function will render the block in the editor. It should return a React component. Use WordPress components like TextControl, NumberControl, URLInput, and ColorPicker to create the UI for the block's attributes.**
-- **Create Save Function: This function will render the block on the front end. It should return a JSX markup.**
+```
+// For this an array of objects will do the job, and the length of that array defines the number of single blocks hosted
+blocks:[{singleBlockAttrs},{singleBlockAttrs},...]
+```
--- src/edit.js
+++ src/edit.js
@@ -1,43 +1,200 @@
 import { useBlockProps, RichText, InspectorControls, ColorPalette } from '@wordpress/block-editor';
-import { PanelBody, TextControl, SelectControl } from '@wordpress/components';
+import { PanelBody, TextControl, SelectControl, ToggleControl } from '@wordpress/components';
 import { Button } from '@wordpress/components';
 import './editor.scss';
+import { useState } from '@wordpress/element';
 
 export default function Edit({ attributes, setAttributes }) {
-  const { price, planName, benefits, discount, link } = attributes;
+  const { price, planName, benefits, discount, link, textStyles, featured, featuredText, description } = attributes;
 
+  const doted = textStyles?.dottedList;
+  const alignList = textStyles?.alignList;
+  const alignPrice = textStyles?.alignPrice;
+  const [showTestPanel, setShowTestPanel] = useState(false);
+
+  const onTextStyleChanges = (type, value) => {
+    const newStyles = { ...textStyles, [type]: value };
+    setAttributes({ textStyles: newStyles });
+  };
+
+  const PlanNameSetting = () => {
+    return (
+      <PanelBody title={'Plan Name Typography'}>
+        <SelectControl
+          label={'Font size'}
+          value={textStyles?.planNameTag}
+          options={[
+            { label: 'Large', value: 'h1' },
+            { label: 'Medium', value: 'h2' },
+            { label: 'Small', value: 'h3' },
+          ]}
+          onChange={(newSize) => {
+            onTextStyleChanges('planNameTag', newSize);
+          }}
+        />
+      </PanelBody>
+    );
+  };
+  const onChangePlanName = (newPlanName) => setAttributes({ planName: newPlanName });
+  const onChangePrice = (newPrice) => {
+    setAttributes({ price: newPrice.replace('$', '') });
+  };
+  const onChangeBenefits = (newBenefits) => setAttributes({ benefits: newBenefits });
+  const onChangeDiscount = (newDiscount) => {
+    setAttributes({ discount: newDiscount.replace('$', '') });
+  };
+  const onChangeLink = (newLink) => setAttributes({ link: newLink });
+  const onChangeFeaturedText = (newFeaturedText) => setAttributes({ featuredText: newFeaturedText });
+  const onChangeDescription = (newDescription) => setAttributes({ description: newDescription });
+
+  const PlanAndPriceTag = ({ place }) => {
+    return (
+      <>
+        {place == 'top' ? (
+          <>
+            <RichText
+              className="price-box-plan-name"
+              tagName={textStyles?.planNameTag || 'h1'}
+              value={planName}
+              onClick={() => {
+                setShowTestPanel(!showTestPanel);
+              }}
+              onChange={onChangePlanName}
+            />
+            <div
+              className="price-tag-wrapper"
+              style={{
+                position: discount ? 'relative' : '',
+                right: discount ? '17px' : '0px',
+              }}
+            >
+              {discount && <RichText className="price-tag-initial" tagName="p" value={`$${discount}`} onChange={onChangeDiscount} />}
+              <RichText className="price-tag" tagName="h1" value={`$${price}`} onChange={onChangePrice} />
+            </div>
+          </>
+        ) : (
+          <>
+            <div
+              className="price-tag-wrapper"
+              style={{
+                position: discount ? 'relative' : '',
+                right: discount ? '17px' : '0px',
+              }}
+            >
+              {discount && <RichText className="price-tag-initial" tagName="p" value={`$${discount}`} onChange={onChangeDiscount} />}
+              <RichText className="price-tag" tagName="h1" value={`$${price}`} onChange={onChangePrice} />
+            </div>
+            <RichText
+              className="price-box-plan-name"
+              tagName={textStyles?.planNameTag || 'h1'}
+              value={planName}
+              onClick={() => {
+                setShowTestPanel(!showTestPanel);
+              }}
+              onChange={onChangePlanName}
+            />
+          </>
+        )}
+      </>
+    );
+  };
   return (
     <div {...useBlockProps()}>
       <InspectorControls>
+        {showTestPanel && <PlanNameSetting />}
+
         <PanelBody title={'Price Box Settings'}>
-          <TextControl label={'Plan Name'} value={planName} onChange={(newPlanName) => setAttributes({ planName: newPlanName })} />
-          <TextControl label={'Price'} value={price} onChange={(newPrice) => setAttributes({ price: newPrice })} />
-          <TextControl label={'Benefits'} value={benefits} onChange={(newBenefits) => setAttributes({ benefits: newBenefits })} />
-          <TextControl label={'Discount'} value={discount} onChange={(newDiscount) => setAttributes({ discount: newDiscount })} />
-          <TextControl label={'Link'} value={link} onChange={(newLink) => setAttributes({ link: newLink })} />
+          <ToggleControl label={'Featured'} checked={featured} onChange={(newfeatured) => setAttributes({ featured: newfeatured })} />
+          {featured && (
+            <TextControl
+              label={'Featured Text'}
+              value={featuredText}
+              onChange={(newFeaturedText) => setAttributes({ featuredText: newFeaturedText })}
+            />
+          )}
+
+          <TextControl label={'Plan Name'} value={planName} onChange={onChangePlanName} />
+          <SelectControl
+            label={'Plan Placement'}
+            value={textStyles?.planPlacement}
+            options={[
+              { label: 'top', value: 'top' },
+              { label: 'bottom', value: 'bottom' },
+            ]}
+            onChange={(newAlignList) => onTextStyleChanges('planPlacement', newAlignList)}
+          />
+          <TextControl label={'Price'} value={price} onChange={onChangePrice} />
+          <TextControl label={'Benefits'} value={benefits} onChange={onChangeBenefits} />
+          <TextControl label={'Discount'} value={discount} onChange={onChangeDiscount} />
+          <TextControl label={'Link'} value={link} onChange={onChangeLink} />
+
+          <ToggleControl
+            label={'Dotted List'}
+            checked={doted}
+            onChange={(newDottedList) => onTextStyleChanges('dottedList', newDottedList)}
+          />
+
+          <SelectControl
+            label={'Align List'}
+            value={alignList}
+            options={[
+              { label: 'Left', value: 'flex-start' },
+              { label: 'Center', value: 'center' },
+              { label: 'Right', value: 'flex-end' },
+            ]}
+            onChange={(newAlignList) => onTextStyleChanges('alignList', newAlignList)}
+          />
         </PanelBody>
       </InspectorControls>
-      <div className="price-box-container">
-        <RichText.Content tagName="h2" value={planName} />
-        <div
-          className="price-tag-wrapper"
-          style={{
-            right: discount ? '17px' : '0px',
-            position: discount ? 'relative' : '',
-          }}
-        >
-          {discount && <RichText.Content className="price-tag-initial" tagName="p" value={`$${discount}`} />}
-          <RichText.Content className="price-tag" tagName="h1" value={`$${price}`} />
-        </div>
+
+      <div className={`price-box-container ${featured ? 'featured-block' : 'easy-theme'}`}>
+        {featured && (
+          <div className="featured-tag">
+            <RichText tagName="p" value={featuredText} onChange={onChangeFeaturedText} />
+          </div>
+        )}
+
+        <PlanAndPriceTag place={textStyles?.planPlacement} />
+        <RichText className="price-description" tagName="p" value={description} onChange={onChangeDescription} />
         {benefits && benefits.length > 0 && (
-          <ul className="price-box-ul">
-            {benefits.map((benefit) => (
-              <RichText.Content tagName="li" value={benefit} />
+          <ul
+            className="price-box-ul"
+            style={{
+              listStyle: doted ? 'disc' : 'none',
+              alignItems: alignList,
+            }}
+          >
+            {benefits.map((benefit, i) => (
+              <RichText
+                tagName="li"
+                value={benefit}
+                onChange={(newBenefit) => {
+                  // if empty, delete from array
+                  if (!newBenefit) {
+                    const newBenefits = [...benefits];
+                    newBenefits.splice(i, 1);
+                    onChangeBenefits(newBenefits);
+                    return;
+                  }
+                  const newBenefits = [...benefits];
+                  newBenefits[i] = newBenefit;
+                  onChangeBenefits(newBenefits);
+                }}
+              />
             ))}
+            {/* plugs button that adds an element to the benifits */}
+            <Button
+              variant="link"
+              onClick={() => {
+                onChangeBenefits([...benefits, 'New Benefit']);
+              }}
+            >
+              + Add Benefit
+            </Button>
           </ul>
         )}
         <a href={link}>
-          <Button variant="secondary">Buy Now</Button>
+          <Button variant="primary">Buy Now</Button>
         </a>
       </div>
     </div>
